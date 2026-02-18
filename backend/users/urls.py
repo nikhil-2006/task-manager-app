2e3138e07b8300292418f69bb01986f0cdb105bc
Nikhil <jjnikhil113@gmail.com>
2026-02-19 00:49:18 +0530

    Fixed missing path import

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from users.views import RegisterView, ProfileView
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-from users.views import RegisterView, ProfileView
-
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
     path('api/auth/register/', RegisterView.as_view()),
     path('api/auth/profile/', ProfileView.as_view()),
+
+    # 🔥 ADD THESE TWO LINES
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
