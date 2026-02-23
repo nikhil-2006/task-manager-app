ff7284f58ad2a1f28fd36e8e59ebb58aad8d398c
nikhil <jjnikhil113@gmail.com>
2026-02-23 23:31:41 +0530

    Initialised the commit

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,18 +1,14 @@
-from django.contrib import admin
 from django.urls import path
-from users.views import RegisterView, ProfileView
-
+from .views import RegisterView, ProfileView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/profile/', ProfileView.as_view(), name='profile'),
 
-    path('auth/register/', RegisterView.as_view(), name = 'register'),
-    path('auth/profile/', ProfileView.as_view()),
-
-    # 🔥 ADD THESE TWO LINES
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
